ff5053990e0f23c5da6aa5826e33f60965e7ad2b
Keeley Duffey <keeleyduffey@gmail.com>
2016-07-07 13:45:39 -0400

    Problem 1 Solved

--- name_sorter.rb
+++ name_sorter.rb
@@ -15,3 +15,50 @@
 #       ex:   1. Adam L.
 #             2. Bob R.
 ##
+
+
+
+names = ['chris juFfey', 'Linda ruffey', 'Amelia lunch', 'Billy Bunch', 'Harrison robe', 'J.T. LObe', 'Brantlee Dobe', 'Debbie Cobe', 'Gary Jobe', 'Keeley Duffey']
+
+def solution(array)
+	
+	formatted_names = []
+
+	array.each do |name|
+		both = name.split
+		first = both[0].capitalize        #Chris
+		last = both[1].capitalize[0] + '.'  #J. 
+		name = first + ' ' + last
+		formatted_names << name
+	end
+ 
+ n = 1
+
+ formatted_names.sort.each do |name|
+ 	puts n.to_s + '. ' + name
+ 	n += 1
+ end
+
+puts
+
+# 	n = 1 
+# 	array.sort.each do |person|
+# 		person= person.split
+# 		puts "#{n} #{person}"
+# 		n +=1
+
+# 		person.each do |first_name|
+# 			first_name = first_name.
+# 			puts first_name = person.first_name 
+# 		end
+	
+# 	end
+
+end
+
+solution(names)
+
+
+
+
+
